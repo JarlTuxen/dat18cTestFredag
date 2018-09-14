d4f9ced6ea6bcb4eaad440b3d840cb4dfe239b96
JarlTuxen <jart@kea.dk>
2018-09-14 13:14:48 +0200

    første commit

---
+++ Test.java
@@ -0,0 +1,20 @@
+public class Test{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+    String a;
+
+    public void set(String x)
+    {
+        a = x;
+    }
+
+    public String get()
+    {
+        return a;
+    }
+
+
+}
